3454aab3a41a18633155cf58921202bda67ebe76
Vishal Kuo <vishalkuo@hotmail.com>
2016-06-09 23:24:36 -0400

    Add help feature and refactor for better modularity

--- bot/commands/doorcode.py
+++ bot/commands/doorcode.py
@@ -10,11 +10,26 @@ Finds the doorcode if it exists for a particular query
 class Doorcode(Base_Command):
     def __init__(self, s):
         self.s = s
-        self.match_patten = '[0-9]{3}'
+        self.match_patten = re.compile('[0-9]{3}')
+        self.help_pattern = re.compile('-help')
+        self.help_str = help_str = 'Produces the doorcode for a' \
+        'specific class (three digit code), '\
+        'just include \'doorcode\' or \'door code\' with a three '\
+        'digit code in your request'
 
     def post_message(self, m):
+        if self.help_pattern.search(self.s):
+            commands.util.post(self.help_str, m)
+        else:
+            doorcode_str = self.post_doorcode(m)
+            commands.util.post(doorcode_str, m)
+        
+
+    def post_doorcode(self, m):
         ret_str = ''
         nums = re.findall(self.match_patten, m.get('text'))
+        if len(nums) < 1:
+            return 'Sorry, your doorcode seems to be invalid'
         nums = nums[0]
         conn = sqlite3.connect('bot/commands/JARVIS')
         nums = (nums, )
@@ -24,6 +39,8 @@ class Doorcode(Base_Command):
             ret_str = '`{0}`'.format(ret[0])
         else:
             ret_str = 'Sorry, I couldn\'t find a doorcode for {0}!'.format(nums[0])  
-        commands.util.post(ret_str, m)
+        return ret_str
+   
+        
 
        
