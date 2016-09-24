bdd54baa93fc7700cb010a91244c98ad73fecad7
Vishal Kuo <vishalkuo@hotmail.com>
2016-09-24 09:20:15 -0400

    Built http version

--- main.go
+++ main.go
@@ -6,12 +6,25 @@ import(
   "os"
   "io/ioutil"
   "strings"
+  "net/http"
+  "fmt"
 )
+
+func spawn_server(){
+  port := ":" + os.Getenv("PORT")
+  http.HandleFunc("/", web_handler)
+  http.ListenAndServe(port, nil)
+}
+
+func web_handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main(){
   logger := log.New(os.Stdout, "Jarvis: ", log.Lshortfile|log.LstdFlags)
   api := initialize(logger)
   rtm := api.NewRTM()
+  go spawn_server()
   go rtm.ManageConnection()
   for {
     select {
